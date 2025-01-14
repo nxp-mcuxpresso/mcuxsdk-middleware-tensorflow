c178c26899f4fac9675bf243bfa593584df54654
Xiaodong Xie <xiaodong.xie@nxp.com>
2025-01-15 06:58:44 +0800

    [AIR-12464] Update tflm library for rt595, rt685
    Signed-off-by: Xiaodong Xie <xiaodong.xie@nxp.com>

---
+++ lib/hifi4/xcc/rt685s/build.sh
@@ -0,0 +1,14 @@
+export PATH="$PATH:/opt/xtensa/XtDevTools/install/tools/RI-2023.11-linux/XtensaTools/bin"
+export XTENSA_BASE="opt/xtensa/XtDevTools/install"
+export XTENSA_SYSTEM="/opt/xtensa/XtDevTools/install/builds/RI-2023.11-linux/nxp_rt600_RI23_11_newlib/config"
+export XTENSA_TOOLS_VERSION="RI-2023.11-linux"
+export XTENSA_CORE="nxp_rt600_RI23_11_newlib"
+
+git clone https://github.com/tensorflow/tflite-micro.git tflite-micro-2024-08-07-d3475aa
+cd tflite-micro-2024-08-07-d3475aa
+git checkout d3475aa
+git apply ../tflm.patch
+make -f tensorflow/lite/micro/tools/make/Makefile BUILD_TYPE=release_with_logs TARGET=xtensa TARGET_ARCH=hifi4 XTENSA_USE_LIBC=true OPTIMIZED_KERNEL_DIR=xtensa microlite
+cp gen/xtensa_hifi4_release_with_logs_xtensa_gcc/lib/libtensorflow-microlite.a ../libtflm.a
+cd ..
+
