4e49f04241d15e25d0f1107d8523acfc830cfb1a
X.D. Xie <xiaodong.xie@nxp.com>
2025-05-15 10:47:05 +0200

    Pull request #282: [AIR-12986] Upgrade TFLiteMicro to 2025-04-08
    Merge in AITEC/tensorflow-lite-imx-rt from main-dev to main

--- lib/fusionf1/xcc/rt595s/build.sh
+++ lib/fusionf1/xcc/rt595s/build.sh
@@ -4,11 +4,9 @@ export XTENSA_SYSTEM="/opt/xtensa/XtDevTools/install/builds/RI-2023.11-linux/nxp
 export XTENSA_TOOLS_VERSION="RI-2023.11-linux"
 export XTENSA_CORE="nxp_rt500_RI23_11_newlib"
 
-git clone https://github.com/tensorflow/tflite-micro.git tflite-micro-2024-08-07-d3475aa
-cd tflite-micro-2024-08-07-d3475aa
-git checkout d3475aa
+git clone https://github.com/tensorflow/tflite-micro.git tflite-micro
+cd tflite-micro
+git checkout bc68d362d6f3ac93ce11d8712974d05b1d6a8305
 git apply ../tflm.patch
 make -f tensorflow/lite/micro/tools/make/Makefile BUILD_TYPE=release_with_logs TARGET=xtensa TARGET_ARCH=hifi4 XTENSA_USE_LIBC=true OPTIMIZED_KERNEL_DIR=xtensa microlite
 cp gen/xtensa_hifi4_release_with_logs_xtensa_gcc/lib/libtensorflow-microlite.a ../libtflm.a
-cd ..
-
