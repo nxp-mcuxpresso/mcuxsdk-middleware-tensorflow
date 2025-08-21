f3e07bff9994c2d5276b78ea73edfb8972389b75
X.D. Xie <xiaodong.xie@nxp.com>
2025-08-21 19:05:27 +0200

    Pull request #293: [AIR-13344] Upgrade tflm library for hifi dsp
    Merge in AITEC/tensorflow-lite-imx-rt from AIR-13344-upgrade-tflm-library-for-hifi-dsp to main

--- lib/hifi4/xcc/rt685s/build.sh
+++ lib/hifi4/xcc/rt685s/build.sh
@@ -4,9 +4,9 @@ export XTENSA_SYSTEM="/opt/xtensa/XtDevTools/install/builds/RI-2023.11-linux/nxp
 export XTENSA_TOOLS_VERSION="RI-2023.11-linux"
 export XTENSA_CORE="nxp_rt600_RI23_11_newlib"
 
-git clone https://github.com/tensorflow/tflite-micro.git tflite-micro
+git clone https://github.com/cad-audio/tflite-micro.git tflite-micro
 cd tflite-micro
-git checkout 3200ccd18268346d0ea965720c5599a3d051e853
+git checkout 54331e9fe42af1dcc8221d693ae85890fb399944
 git apply ../tflm.patch
 make -f tensorflow/lite/micro/tools/make/Makefile BUILD_TYPE=release_with_logs TARGET=xtensa TARGET_ARCH=hifi4 XTENSA_USE_LIBC=true OPTIMIZED_KERNEL_DIR=xtensa microlite
 cp gen/xtensa_hifi4_release_with_logs_xtensa_gcc/lib/libtensorflow-microlite.a ../libtflm.a
