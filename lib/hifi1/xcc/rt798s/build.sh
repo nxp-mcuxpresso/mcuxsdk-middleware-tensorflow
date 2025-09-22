76e0d7972b1ee59d4c31304225f8f94d7ea492f3
X.D. Xie <xiaodong.xie@nxp.com>
2025-09-22 07:30:28 +0200

    Pull request #294: [AIR-13457] update hifi1 library for rt700
    Merge in AITEC/tensorflow-lite-imx-rt from AIR-13457-update-hifi1-library-for-rt700 to main

--- lib/hifi1/xcc/rt798s/build.sh
+++ lib/hifi1/xcc/rt798s/build.sh
@@ -1,12 +1,11 @@
-export PATH="$PATH:/opt/xtensa/XtDevTools/install/tools/RI-2023.11-linux/XtensaTools/bin"
-export XTENSA_BASE="opt/xtensa/XtDevTools/install"
+export XTENSA_BASE="/opt/xtensa/XtDevTools/install"
 export XTENSA_SYSTEM="/opt/xtensa/XtDevTools/install/builds/RI-2023.11-linux/rt700_hifi1_RI23_11_nlib/config"
 export XTENSA_TOOLS_VERSION="RI-2023.11-linux"
 export XTENSA_CORE="rt700_hifi1_RI23_11_nlib"
 
-git clone https://github.com/tensorflow/tflite-micro.git tflite-micro
+git clone https://github.com/cad-audio/tflite-micro.git tflite-micro
 cd tflite-micro
-git checkout 3200ccd18268346d0ea965720c5599a3d051e853
+git checkout 54331e9fe42af1dcc8221d693ae85890fb399944
 git apply ../tflm.patch
 make -j8 -f tensorflow/lite/micro/tools/make/Makefile BUILD_TYPE=release_with_logs TARGET=xtensa TARGET_ARCH=hifi4 XTENSA_USE_LIBC=true OPTIMIZED_KERNEL_DIR=xtensa microlite
 cp gen/xtensa_hifi4_release_with_logs_xtensa_gcc/lib/libtensorflow-microlite.a ../libtflm.a
